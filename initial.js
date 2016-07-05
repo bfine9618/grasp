db32e1c6c063be8054ac952be8ec47eb693c9759
Andreas Wang <andreasx@seas.upenn.edu>
2016-07-04 22:51:07 -0400

    screens need to be connected via navigator

---
+++ initial.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {
+  Navigator,
+  Text,
+  View,
+  Image,
+  TextInput, 
+  TouchableHighlight
+} from 'react-native';
+
+var styles = require('./styles');
+
+export default class initial extends Component{
+	constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: true
+    };
+  }
+  render() {
+    return (
+    	<View style={styles.container}>
+        <Image
+          style = { styles.icon }
+          source={require("../images/Logo1.png")} 
+        />
+        <Text style={styles.heading}>
+          Hi there! I'm Ace 
+         </Text>
+         <Text style={styles.caption}>
+         What do you need to grasp today?
+         </Text>
+         <TextInput
+          style={styles.wideInput}
+          onChangeText={(text) => this.setState({coursecode : text})}
+          value={this.state.coursecode}
+          placeholder="course code"
+        /> 
+        <TouchableHighlight 
+          style={styles.fullWidthButton}
+          activeOpacity={0.6}
+          underlayColor={'purple'}
+          onPress={() => console.log("Find")}>
+        <Text style={styles.fullWidthButtonText}>Find me a Tutor!</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
---
+++ loading.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  Navigator,
+  Text,
+  View,
+  Image,
+  TextInput, 
+  TouchableHighlight
+} from 'react-native';
+
+var styles = require('./styles');
+var ProgressBar = require('ProgressBarAndroid')
+export default class loading extends Component{
+	constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: true
+    };
+  }
+  
+  render () {
+    var progressBar =
+      <View style={styles.container}>
+        <ProgressBar styleAttr="Inverse" />
+      </View>;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.caption}>
+        {"\n"} {"\n"} {"\n"} {"\n"} {"\n"} {"\n"} {"\n"} {"\n"}
+        {"\n"}{"\n"}{"\n"}{"\n"}
+        </Text>
+        <ProgressBar styleAttr="Large" color="blue"/>
+         <Text style={styles.heading}>
+         {"\n"}{"\n"}
+         We're finding you a tutor.
+         </Text>
+      </View>
+    );
+  }
+}
